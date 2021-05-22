e786529922850bb3c10a03d732aa48b004a8152b
gacheri <michenibrenda81@gmail.com>
2021-05-22 13:39:53 +0300

    Question 1 and 2

---
+++ Q2/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    #login{
+        padding: 2em;
+    }
+    #login input{
+        margin-bottom: 1em;
+        height: 2em;
+    }
+</style>
+<body>
+    <div>
+        <form action="" id='login'>
+            <h4>PLEASE ENTER YOUR LOGIN DETAILS HERE</h4>
+            <p>username</p>
+            <input type="text" name="username"><br>
+            <p>password</p>
+            <input type="password"> <br>
+            <a href="vuvuzela.php">Submit</a>
+        </form>
+    </div>
+</body>
+</html>
---
+++ Q2/vuvuzela.php
@@ -0,0 +1,20 @@
+<html>  
+<body>  
+<form method="post">  
+Enter First Number:  
+<input type="number" name="a" /><br><br>  
+Enter Second Number:  
+<input type="number" name="b" /><br><br>  
+<input  type="submit" name="submit" value="Add">  
+</form>  
+<?php  
+    if(isset($_POST['submit']))  
+    {  
+        $number1 = $_POST['a'];  
+        $number2 = $_POST['b'];  
+        $sum =  $number1+$number2;     
+echo "The sum of $number1 and $number2 is: ".$sum;   
+}  
+?>  
+</body>  
+</html> 
